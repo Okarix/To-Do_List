48cce6cea56a0d5bc8e0236bc79e7fd5dd63f1da
Adiar <93129221+Okarix@users.noreply.github.com>
2023-12-08 18:02:03 +0600

    added switching between buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import ToDoBar from './components/ToDoBar';
 import ToDoInput from './components/ToDoInput';
 import ToDoList from './components/ToDoList';
 
 function App() {
 	return (
-		<>
+		<BrowserRouter>
 			<ToDoBar />
 			<ToDoInput />
 			<ToDoList />
-		</>
+		</BrowserRouter>
 	);
 }
 
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,12 @@
 import { Box, Button } from '@mui/material';
 import ToDo from './ToDo';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import ToDoDone from './ToDoDone';
+import { useState } from 'react';
 
 export default function ToDoList() {
+	const [activeBtn, setActiveBtn] = useState('/');
+
 	return (
 		<Box
 			sx={{
@@ -18,24 +23,42 @@ export default function ToDoList() {
 			color='primary'
 		>
 			<div style={{ display: 'flex', justifyContent: 'center' }}>
-				<Button
-					variant='outlined'
-					size='large'
-					sx={{
-						marginRight: '5px',
-					}}
-				>
-					To Do
-				</Button>
-				<Button
-					variant='outlined'
-					size='large'
-					sx={{}}
-				>
-					Done
-				</Button>
+				<NavLink to='/'>
+					<Button
+						variant='outlined'
+						size='large'
+						sx={{
+							marginRight: '5px',
+							backgroundColor: activeBtn === '/' ? 'rgba(25, 118, 210, 0.5)' : '',
+						}}
+						onClick={() => setActiveBtn('/')}
+					>
+						To Do
+					</Button>
+				</NavLink>
+				<NavLink to='/done'>
+					<Button
+						variant='outlined'
+						size='large'
+						onClick={() => setActiveBtn('/done')}
+						sx={{
+							backgroundColor: activeBtn === '/done' ? 'rgba(25, 118, 210, 0.5)' : '',
+						}}
+					>
+						Done
+					</Button>
+				</NavLink>
 			</div>
-			<ToDo />
+			<Routes>
+				<Route
+					path='/'
+					element={<ToDo />}
+				/>
+				<Route
+					path='/done'
+					element={<ToDoDone />}
+				/>
+			</Routes>
 		</Box>
 	);
 }
