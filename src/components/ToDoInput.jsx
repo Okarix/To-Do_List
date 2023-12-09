41e34687b3bf30884cad4c583a0a6c2749dec0fc
Adiar <93129221+Okarix@users.noreply.github.com>
2023-12-09 13:19:10 +0600

    added states for saving values ​​from inputs

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -1,8 +1,24 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useState } from 'react';
 
 export default function ToDoInput() {
+	const [todo, setTodo] = useState({
+		title: '',
+		descr: '',
+	});
+
+	const handleTitleChange = event => {
+		setTodo({ ...todo, title: event.target.value });
+		console.log(todo);
+	};
+
+	const handleDescrChange = event => {
+		setTodo({ ...todo, descr: event.target.value });
+		console.log(todo);
+	};
+
 	return (
 		<Box
 			sx={{
@@ -25,6 +41,8 @@ export default function ToDoInput() {
 						width: '250px',
 						marginRight: '10px',
 					}}
+					onChange={handleTitleChange}
+					value={todo.title}
 				/>
 				<TextField
 					label='Create Description'
@@ -32,6 +50,8 @@ export default function ToDoInput() {
 						width: '250px',
 						marginRight: '20px',
 					}}
+					onChange={handleDescrChange}
+					value={todo.descr}
 				/>
 				<AddCircleIcon
 					color='primary'
